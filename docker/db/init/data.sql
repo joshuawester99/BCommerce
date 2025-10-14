8cd98e431911188d285301662325bceafc9bcc97
AgentArgon <Joshua_Wester1@baylor.edu>
2025-10-14 06:09:22 -0500

    Fixed pom.xmls

--- docker/db/init/data.sql
+++ docker/db/init/data.sql
@@ -1,8 +1,4 @@
-INSERT INTO public.users (name, email, credit_card, address)
-VALUES 
-('C. S. Lewis', 'cslewis@narnia.com', '1234123412341234', 'Narnia'),
-('Joshua Wester', 'joshua.wester@baylor.edu', '4321432143214321', 'Kansas');
+INSERT INTO public.users (name, email, credit_card, address) VALUES ('C. S. Lewis', 'cslewis@narnia.com', '1234123412341234', 'Narnia');
+INSERT INTO public.users (name, email, credit_card, address) VALUES ('Joshua Wester', 'joshua.wester@baylor.edu', '4321432143214321', 'Kansas');
 
-INSERT INTO public.items (name, description, quantity_in_stock, price)
-VALUES 
-('Pencils', '12-Pack of Yellow, No. 2 Pencils', 5, 3.14);
+INSERT INTO public.items (name, description, quantity_in_stock, price) VALUES ('Pencils', '12-Pack of Yellow, No. 2 Pencils', 5, 3.14);
